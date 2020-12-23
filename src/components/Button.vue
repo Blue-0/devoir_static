1b62a09cd400f05b2a079c9335a7939653007c73
Blue-0 <58938722+Blue-0@users.noreply.github.com>
2020-12-23 22:58:57 +0100

    V3.1

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <a class="button">
+    <slot/> ❯
+  </a>
+</template>
+
+<script>
+import Button from '@/components/Elements/Button'
+
+export default {
+  components: {
+    Button
+  },
+  data() {
+    return {
+      settings: require("../../data/theme.json")
+    }
+  }
+}
+</script>
+
+<style>
+.button {
+  display: inline-block;
+  text-transform: uppercase;
+  text-decoration: none;
+  padding: .75em 1em;
+  border-radius: 20px;
+}
+.is-dark {
+  background: black;
+  color: white;
+}
+</style>
